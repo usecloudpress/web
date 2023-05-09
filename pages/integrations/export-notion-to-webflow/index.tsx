fc37905df01d06bd7cde23622aecdf4ffd187b1f
Jerrie Pelser <jerrie@jerriepelser.com>
2023-05-09 16:47:38 +0700

    Updates Notion to Webflow landing page with latest features

--- pages/integrations/export-notion-to-webflow/index.tsx
+++ pages/integrations/export-notion-to-webflow/index.tsx
@@ -2,14 +2,18 @@ import Layout from "../../../components/layouts/Layout";
 import { NextSeo } from "next-seo";
 import imageCompare1 from "../../../public/images/terracotta-army-notion.png";
 import imageCompare2 from "../../../public/images/terracotta-army-webflow.png";
-import imageWrite from "./write.png";
-import imageExport from "./export.png";
-import imageSchemaConfiguration from "./schema-mapping.png";
-import imageFormatting from "./formatting.png";
-import IntegrationLandingPageHeader from "../../../components/integration-landing-page-header";
+import BulkExportImage from "./bulk-export-documents-from-notion-to-webflow.png";
+import CollectionSchemaImage from "./configure-your-cms-collection-schema.png";
+import EmbedsImage from "./converts-notion-embeds-to-webflow.png";
+import DraftOrPublishedImage from "./export-as-draft-or-published.png";
+import ExportImagesImage from "./export-images-from-notion-to-webflow.png";
+import MapPropertiesImage from "./map-notion-properties-to-webflow-fields.png";
+import PreserveFormattingImage from "./preserves-formatting.png";
+import QuicklyNavigateImage from "./quickly-navigate-to-exported-content-in-webflow.png";
 import IntegrationCta from "../../../components/integration-cta";
+import IntegrationHeroSection from "../../../components/integration-hero-section";
 import FeatureBlockWithImage from "../../../components/feature-block-with-image";
-import FeatureListItem from "../../../components/feature-list-item";
+import FeatureList from "../../../components/feature-list";
 
 export default function Page() {
   const source = "notion";
@@ -17,122 +21,185 @@ export default function Page() {
 
   return (
     <Layout>
-      <NextSeo title="Export content from Notion to Webflow" />
-      <IntegrationLandingPageHeader
+      <NextSeo
+        title="Export perfectly formatted content from Notion to Webflow"
+        description="Automate the export of perfectly formatted content from Notion to Webflow, so you have more time for doing work that matters"
+      />
+      <IntegrationHeroSection
         source={source}
         destination={destination}
         imageCompare1={imageCompare1}
         imageCompare2={imageCompare2}
       />
 
-      <div className="py-16 bg-gradient-to-b from-brand-100 to-white overflow-hidden lg:pb-24">
-        {/* Write in Notion */}
-        <FeatureBlockWithImage
-          image={imageWrite}
-          imageAlignment="left"
-          title={
-            <>
-              Write and collaborate{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                in Notion
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                We understand that you love the block-based editor and
-                collaboration features of Notion and may prefer it over the
-                content editing experience in Webflow.
-              </p>
-              <p>With Cloudpress, you can keep using the tools you love ❤</p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Preserves your formatting"
+        content={
+          <>
+            <p>
+              Cloudpress does a great job of preserving your formatting and
+              handles all common formatting styles, such as headings, bold,
+              italic, and underlined text, lists, tables, code blocks, and more…
+            </p>
+            <FeatureList
+              features={[
+                "Supports all the common formatting styles",
+                "Supports tables with headers",
+                "Supports advanced formatting like code blocks - ideal for programmers that share source code on their blogs",
+              ]}
+            />
+          </>
+        }
+        image={PreserveFormattingImage}
+        imageAlignment="right"
+      />
 
-        {/* Export with one click */}
-        <FeatureBlockWithImage
-          image={imageExport}
-          imageAlignment="right"
-          title={
-            <>
-              Export with{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                one click
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                Using the Cloudpress web app, you can export your content with
-                the click of a button.
-              </p>
-              <p>
-                If you make changes to the Notion document you can easily export
-                it again, and Cloudpress will update the content of the
-                previously exported item in Webflow.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Exports your images"
+        content={
+          <>
+            <p>
+              Cloudpress uploads all the images in your document to Webflow and
+              will link the uploaded images correctly in the exported content.
+            </p>
+            <FeatureList
+              features={[
+                "Preserves your image name for SEO purposes",
+                "Supports alt text for SEO purposes",
+              ]}
+            />
+          </>
+        }
+        image={ExportImagesImage}
+        imageAlignment="left"
+      />
 
-        {/* Works with your Schema */}
-        <FeatureBlockWithImage
-          image={imageSchemaConfiguration}
-          imageAlignment="left"
-          title={
-            <>
-              Works with{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                your schema
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                Cloudpress works with your collection schema. Simply connect
-                your Webflow account, tell Cloudpress which collection and field
-                to use for the content of the exported document, and we’ll take
-                care of the rest.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Works with your CMS collection"
+        content={
+          <>
+            <p>
+              Cloudpress works with your CMS collection schema. Connect your
+              Webflow account, tell Cloudpress which CMS collection and field to
+              use for the content of the exported document, and we’ll take care
+              of the rest
+            </p>
+            <FeatureList
+              features={[
+                "Works with any of your CMS collections",
+                "Cloudpress exports the content to any rich text field in your CMS collection",
+              ]}
+            />
+          </>
+        }
+        image={CollectionSchemaImage}
+        imageAlignment="right"
+      />
 
-        {/* Preserve your formatting */}
-        <FeatureBlockWithImage
-          image={imageFormatting}
-          imageAlignment="right"
-          title={
-            <>
-              We{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                understand
-              </span>{" "}
-              Notion blocks
-            </>
-          }
-          content={
-            <>
-              <p>
-                Cloudpress understands how to work with Notion blocks and
-                convert them to the correct format for Webflow. It handles all
-                the common block types and formatting used in Notion, such as
-              </p>
-              <ul className="space-y-4 mt-5">
-                <FeatureListItem text="Headings" />
-                <FeatureListItem text="Bold text" />
-                <FeatureListItem text="Italic text" />
-                <FeatureListItem text="Tables" />
-                <FeatureListItem text="Lists" />
-                <FeatureListItem text="And more..." />
-              </ul>
-            </>
-          }
-        />
-      </div>
+      <FeatureBlockWithImage
+        title="Handles Notion embeds"
+        content={
+          <>
+            <p>
+              Cloudpress handles Notion embeds correctly and will create the
+              correct embed block for Webflow.
+            </p>
+            <FeatureList
+              features={[
+                "Supports common embeds like Twitter, TikTok, YouTube, Spotify, and many more",
+                "Creates the correct embed code for your content",
+              ]}
+            />
+          </>
+        }
+        image={EmbedsImage}
+        imageAlignment="left"
+      />
+
+      <FeatureBlockWithImage
+        title="Export Notion databases"
+        content={
+          <>
+            <p>
+              Cloudpress allows you to export your entire Notion database to
+              Webflow. It exports the content, and you can map properties from
+              your database to fields in Webflow.
+            </p>
+            <FeatureList
+              features={[
+                "Supports the majority of Notion property types, including selects and images",
+                "Supports Webflow reference fields and can look up the correct item based on the name",
+                "Allows exporting a subset of pages from the database based on filter criteria",
+              ]}
+            />
+          </>
+        }
+        image={MapPropertiesImage}
+        imageAlignment="right"
+      />
+
+      <FeatureBlockWithImage
+        title="Bulk export documents"
+        content={
+          <>
+            <p>
+              If you have a lot of documents you want to export or update, you
+              can use our bulk export feature in the Cloudpress web application.
+            </p>
+            <FeatureList
+              features={[
+                "Export multiple documents at once",
+                "Specify how each document should be processed",
+              ]}
+            />
+          </>
+        }
+        image={BulkExportImage}
+        imageAlignment="left"
+      />
+
+      <FeatureBlockWithImage
+        title="Draft or published, your choice"
+        content={
+          <>
+            <p>
+              Cloudpress can export your content as drafts or publish the
+              content. In the case of content that is re-exported, Cloudpress
+              can preserve the current publication status of your content.
+            </p>
+            <FeatureList
+              features={[
+                "Exports new content as drafts or publish it",
+                "Preserve the state of existing content or explicitly set it to draft or published",
+              ]}
+            />
+          </>
+        }
+        image={DraftOrPublishedImage}
+        imageAlignment="right"
+      />
+
+      <FeatureBlockWithImage
+        title="Quickly navigate to exported content"
+        content={
+          <>
+            <p>
+              Once your export has been completed, you can easily navigate to
+              the exported content in your CMS to review and publish the
+              content.
+            </p>
+            <FeatureList
+              features={[
+                "Navigate directly to your Webflow designer",
+                "Navigate directly to view the export log",
+              ]}
+            />
+          </>
+        }
+        image={QuicklyNavigateImage}
+        imageAlignment="left"
+      />
+
       <IntegrationCta destination={destination} />
     </Layout>
   );
