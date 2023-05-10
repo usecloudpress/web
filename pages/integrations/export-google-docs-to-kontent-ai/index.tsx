39cef451299ad640aecc346c669654e1fbfb7a72
Jerrie Pelser <jerrie@jerriepelser.com>
2023-05-10 13:18:06 +0700

    Updates Google Docs to Kontent integration page with latest features

--- pages/integrations/export-google-docs-to-kontent-ai/index.tsx
+++ pages/integrations/export-google-docs-to-kontent-ai/index.tsx
@@ -1,18 +1,19 @@
-import Link from "next/link";
 import Layout from "../../../components/layouts/Layout";
 import { NextSeo } from "next-seo";
 import imageCompare1 from "../../../public/images/terracotta-army-google-docs.png";
 import imageCompare2 from "../../../public/images/terracotta-army-kontent-ai.png";
-import imageWrite from "./write.png";
-import imageAddOn from "./add-on.png";
-import imageFormatting from "./formatting.png";
-import imageImages from "./images.png";
-import imageModelConfiguration from "./model-configuration.png";
-import imageReexport from "./reexport.png";
-import IntegrationLandingPageHeader from "../../../components/integration-landing-page-header";
+import AutomateExportsImage from "./automate-exports-from-google-docs-to-wordpress-with-trello.png";
+import BulkExportImage from "./bulk-export.png";
+import ContentModelImage from "./content-model.png";
+import ConvertsFormattingImage from "./converts-google-docs-formatting-to-contentful.png";
+import DraftOrPublishedImage from "./draft-or-published.png";
+import ExportsImagesImage from "./exports-images.png";
+import OneClickExportImage from "./one-click-export.png";
+import QuickNavigationImage from "./quick-navigation.png";
 import IntegrationCta from "../../../components/integration-cta";
+import IntegrationHeroSection from "../../../components/integration-hero-section";
 import FeatureBlockWithImage from "../../../components/feature-block-with-image";
-import FeatureListItem from "../../../components/feature-list-item";
+import FeatureList from "../../../components/feature-list";
 
 export default function Page() {
   const source = "google-docs";
@@ -22,189 +23,180 @@ export default function Page() {
     <Layout>
       <NextSeo title="Export from Google Docs to Kontent.ai" />
 
-      <IntegrationLandingPageHeader
+      <IntegrationHeroSection
         source={source}
         destination={destination}
         imageCompare1={imageCompare1}
         imageCompare2={imageCompare2}
       />
 
-      <div className="py-16 bg-gradient-to-b from-brand-100 to-white overflow-hidden lg:pb-24">
-        {/* Write in Google Docs */}
-        <FeatureBlockWithImage
-          image={imageWrite}
-          imageAlignment="left"
-          title={
-            <>
-              Write in{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                Google Docs
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                Keep using the powerful editing and collaboration features that
-                makes Google Docs such a great writing tool.
-              </p>
-              <p>
-                <Link href="/blog/write-your-next-blog-post-in-google-docs/">
-                  Read more
-                </Link>{" "}
-                about why we think Google Docs is a great choice for writing
-                content.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="One-click exports"
+        content={
+          <>
+            <p>
+              With our Google Docs Add-on, you can export your content from the
+              comfort of the Google Docs editor. No need to navigate to an
+              external application - simply open the Cloudpress Google Docs
+              Add-on and click the export button.
+            </p>
+            <FeatureList
+              features={[
+                "Save time by not leaving the Google Docs editor",
+                "Get updates on the export progress from inside the Google Docs editor",
+                "Navigate directly to your content in Kontent.ai when the export is done",
+              ]}
+            />
+          </>
+        }
+        image={OneClickExportImage}
+        imageAlignment="right"
+      />
 
-        {/* Export with one click */}
-        <FeatureBlockWithImage
-          image={imageAddOn}
-          imageAlignment="right"
-          title={
-            <>
-              Export with{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                one click
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                Using our Google Docs Add-On, you can export your content
-                without ever leaving Google Docs. Just click the export button
-                and wait for the export to finish.
-              </p>
-              <p>
-                Once your export has completed, you can easily navigate to the
-                exported content in Kontent.ai to edit any additional fields or
-                publish the content.
-              </p>
-              <p>
-                <Link href="/docs/integrations/google-docs/introduction">
-                  Learn more
-                </Link>{" "}
-                about our Google Docs Add-on.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Preserves your formatting"
+        content={
+          <>
+            <p>
+              Cloudpress does a great job of preserving your formatting and
+              handles all common formatting styles, such as headings, bold,
+              italic, and underlined text, lists, tables, code blocks, and more…
+            </p>
+            <FeatureList
+              features={[
+                "Supports all the common formatting styles",
+                "Supports tables with headers and text formatting",
+              ]}
+            />
+          </>
+        }
+        image={ConvertsFormattingImage}
+        imageAlignment="left"
+      />
 
-        {/* Works with your Content Model */}
-        <FeatureBlockWithImage
-          image={imageModelConfiguration}
-          imageAlignment="left"
-          title={
-            <>
-              Works with{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                your content model
-              </span>
-            </>
-          }
-          content={
-            <>
-              <p>
-                Cloudpress works with your Content Model. Simply connect your
-                Kontent.ai account, tell Cloudpress which elements to use for
-                the title and content of the exported document, and we’ll take
-                care of the rest.
-              </p>
-              <p>
-                <Link href="/docs/integrations/kontent/connect-account">
-                  Learn how
-                </Link>{" "}
-                to connect your Kontent.ai account.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Exports your images"
+        content={
+          <>
+            <p>
+              Cloudpress uploads all the images in your document as assets to
+              Kontent.ai and will link those assets correctly in the exported
+              content.
+            </p>
+            <FeatureList
+              features={[
+                "Uploads images as assets in Kontent.ai",
+                "Preserves your image name for SEO purposes",
+                "Supports alt text for SEO purposes",
+              ]}
+            />
+          </>
+        }
+        image={ExportsImagesImage}
+        imageAlignment="right"
+      />
 
-        {/* Preserve your formatting */}
-        <FeatureBlockWithImage
-          image={imageFormatting}
-          imageAlignment="right"
-          title={
-            <>
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                Preserve
-              </span>{" "}
-              your formatting
-            </>
-          }
-          content={
-            <>
-              <p>
-                Cloudpress does a great job of preserving your formatting and
-                handles all the common formatting styles used in Google Docs,
-                such as
-              </p>
-              <ul className="space-y-4 mt-5">
-                <FeatureListItem text="Headings" />
-                <FeatureListItem text="Bold text" />
-                <FeatureListItem text="Italic text" />
-                <FeatureListItem text="Tables" />
-                <FeatureListItem text="Lists" />
-                <FeatureListItem text="And more..." />
-              </ul>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Works with your content model"
+        content={
+          <>
+            <p>
+              Cloudpress works with the content models you create in Kontent.ai.
+            </p>
+            <FeatureList
+              features={[
+                "Works with your custom content model",
+                "Allows you to map content to any rich text field",
+              ]}
+            />
+          </>
+        }
+        image={ContentModelImage}
+        imageAlignment="left"
+      />
 
-        {/* Export your images too */}
-        <FeatureBlockWithImage
-          image={imageImages}
-          imageAlignment="left"
-          title={
-            <>
-              Export your{" "}
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                images
-              </span>{" "}
-              too
-            </>
-          }
-          content={
-            <>
-              <p>
-                Cloudpress does not just work with text - your images are taken
-                along for the ride as well. Images are exported as Assets to
-                Kontent.ai and linked correctly in the exported content.
-              </p>
-            </>
-          }
-        />
+      <FeatureBlockWithImage
+        title="Bulk export documents"
+        content={
+          <>
+            <p>
+              If you have a lot of documents you want to export or update, you
+              can use our bulk export feature in the Cloudpress web application.
+            </p>
+            <FeatureList
+              features={[
+                "Export multiple documents at once",
+                "Specify how each document should be processed",
+              ]}
+            />
+          </>
+        }
+        image={BulkExportImage}
+        imageAlignment="right"
+      />
 
-        {/* Re-export content */}
-        <FeatureBlockWithImage
-          image={imageReexport}
-          imageAlignment="right"
-          title={
-            <>
-              <span className="underline underline-offset-2 decoration-brand-500 decoration-4">
-                Re-export
-              </span>{" "}
-              content
-            </>
-          }
-          content={
-            <>
-              <p>
-                Made changes and want to re-export your content from Google
-                Docs?
-              </p>
-              <p>
-                No worries. Cloudpress will update the content of the previously
-                exported content without messing up any of the other fields you
-                may have updated in Kontent.ai.
-              </p>
-            </>
-          }
-        />
-      </div>
+      <FeatureBlockWithImage
+        title="Automate exports"
+        content={
+          <>
+            <p>
+              Using the Cloudpress API and no-code tools like Make.com, you can
+              create automated workflows that export your content without you
+              having to lift a finger.
+            </p>
+            <FeatureList
+              features={[
+                "Cloudpress REST API allows integration from any programming language",
+                "Cloudpress modules allow quick integration with Make.com",
+              ]}
+            />
+          </>
+        }
+        image={AutomateExportsImage}
+        imageAlignment="left"
+      />
+
+      <FeatureBlockWithImage
+        title="Draft or published, your choice"
+        content={
+          <>
+            <p>
+              Cloudpress can export your content as drafts or publish the
+              content. In the case of content that is re-exported, Cloudpress
+              can preserve the current publication status of your content.
+            </p>
+            <FeatureList
+              features={[
+                "Exports new content as drafts or publish it",
+                "Preserve the state of existing content or explicitly set it to draft or published",
+              ]}
+            />
+          </>
+        }
+        image={DraftOrPublishedImage}
+        imageAlignment="right"
+      />
+
+      <FeatureBlockWithImage
+        title="Quickly navigate to exported content"
+        content={
+          <>
+            <p>
+              Once your export has been completed, you can easily navigate to
+              the exported content in Kontent.ai to review and publish the
+              content.
+            </p>
+            <FeatureList
+              features={[
+                "Navigate directly to the exported content in Kontent.ai",
+                "Navigate directly to Cloudpress to review the export logs",
+              ]}
+            />
+          </>
+        }
+        image={QuickNavigationImage}
+        imageAlignment="left"
+      />
 
       <IntegrationCta destination={destination} />
     </Layout>
