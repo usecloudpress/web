ddd701909de7feb1303b045485d7d5b432b9134d
Jerrie Pelser <jerrie@jerriepelser.com>
2023-09-19 12:07:14 +0700

    Replaces markdoc with remark

--- components/markdown/CustomLink.tsx
+++ components/markdown/CustomLink.tsx
@@ -3,10 +3,6 @@ import Link from "next/link";
 type Props = React.HTMLProps<HTMLAnchorElement>;
 
 export default function CustomLink({ children, href = "" }: Props) {
-  const domainRegex = /https:\/\/www\.usecloudpress\.com\//;
-  if (domainRegex.test(href)) {
-    href = href.replace(domainRegex, "/");
-  }
   return href && (href.startsWith("/") || href === "") ? (
     <Link href={href}>
       <>{children}</>
--- components/markdown/helpers.ts
+++ components/markdown/helpers.ts
@@ -1,62 +1,51 @@
-import Markdoc, {
-  nodes as defaultNodes,
-  RenderableTreeNode,
-  Tag,
-} from "@markdoc/markdoc";
-import { slugifyWithCounter } from "@sindresorhus/slugify";
-import { Parser } from "htmlparser2";
+// See https://nextjs.org/learn/basics/dynamic-routes/render-markdown
+// and https://claritydev.net/blog/nextjs-blog-remark-interactive-table-of-contents
+
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeReact from "rehype-react";
+import rehypeRaw from "rehype-raw";
+import { inspectUrls } from "@jsdevtools/rehype-url-inspector";
+import CustomLink from "./CustomLink";
+import React from "react";
 
 export function generateContent(markdown?: string): {
-  content: RenderableTreeNode;
+  content: JSX.Element;
   tableOfContents: TocItem[];
 } {
-  const tokenizer = new Markdoc.Tokenizer({ html: true });
-  const tokens = tokenizer.tokenize(markdown ?? "");
-  const processed = processTokens(tokens);
-  const ast = Markdoc.parse(processed);
-  const content = Markdoc.transform(ast, {
-    nodes: {
-      heading: {
-        ...defaultNodes.heading,
-        attributes: {
-          id: { type: String },
-          level: { type: Number, required: true, default: 1 },
-          className: { type: String },
-        },
-        transform(node, config) {
-          const attributes = node.transformAttributes(config);
-          const children = node.transformChildren(config);
-          const id = generateID(children, attributes);
+  const domainRegex = /https:\/\/www\.usecloudpress\.com\//;
 
-          return new Tag(
-            `h${node.attributes["level"]}`,
-            { ...attributes, id },
-            children
-          );
-        },
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(inspectUrls, {
+      selectors: ["a[href]"],
+      inspectEach({ url, node, propertyName }) {
+        if (node.properties && propertyName) {
+          // Make URLs that start with https://www.usecloudpress.com/ relative (/)
+          if (domainRegex.test(url)) {
+            node.properties[propertyName] = url.replace(domainRegex, "/");
+          }
+        }
       },
-      link: {
-        ...defaultNodes.link,
-        render: "CustomLink",
+    })
+    .use(rehypeReact, {
+      createElement: React.createElement,
+      components: {
+        a: CustomLink,
       },
-    },
-    tags: {
-      "html-tag": {
-        attributes: {
-          name: { type: String, required: true },
-          attrs: { type: Object },
-        },
-        transform(node, config) {
-          const { name, attrs } = node.attributes;
-          const children = node.transformChildren(config);
-          return new Markdoc.Tag(name, attrs, children);
-        },
-      },
-    },
-  });
+    });
 
-  const tableOfContents = collectHeadings(content);
-  return { content, tableOfContents };
+  // const tableOfContents = collectHeadings(content);
+
+  return {
+    content: processor.processSync(markdown).result,
+    tableOfContents: [],
+  };
 }
 
 export interface TocItem {
@@ -66,6 +55,7 @@ export interface TocItem {
   children: TocItem[];
 }
 
+/*
 export function collectHeadings(
   node: RenderableTreeNode,
   sections: TocItem[] = []
@@ -104,67 +94,4 @@ export function collectHeadings(
 
   return sections;
 }
-
-function isTag(item: RenderableTreeNode): item is Tag {
-  return (item as Tag).$$mdtype !== undefined;
-}
-
-function generateID(
-  children: RenderableTreeNode[],
-  attributes: Record<string, any>
-) {
-  if (attributes.id && typeof attributes.id === "string") {
-    return attributes.id;
-  }
-  const slugify = slugifyWithCounter();
-  const nodeText = children
-    .filter((child) => typeof child === "string")
-    .join(" ");
-  return slugify(nodeText);
-}
-
-function processTokens(tokens: any) {
-  const output = [];
-
-  const parser = new Parser({
-    onopentag(name: any, attrs: any) {
-      output.push({
-        type: "tag_open",
-        nesting: 1,
-        meta: {
-          tag: "html-tag",
-          attributes: [
-            { type: "attribute", name: "name", value: name },
-            { type: "attribute", name: "attrs", value: attrs },
-          ],
-        },
-      });
-    },
-
-    ontext(content: any) {
-      if (typeof content === "string" && content.trim().length > 0)
-        output.push({ type: "text", content });
-    },
-
-    onclosetag() {
-      output.push({
-        type: "tag_close",
-        nesting: -1,
-        meta: { tag: "html_close" },
-      });
-    },
-  });
-
-  for (const token of tokens) {
-    if (token.type.startsWith("html")) {
-      parser.write(token.content);
-      continue;
-    }
-
-    if (token.type === "inline") token.children = processTokens(token.children);
-
-    output.push(token);
-  }
-
-  return output;
-}
+*/
