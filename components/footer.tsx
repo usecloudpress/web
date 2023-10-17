b30fcabd9201644e9586dc83d7cc9e4b50b96699
Jerrie Pelser <jerrie@jerriepelser.com>
2023-10-17 09:34:17 +0700

    Updates footer to include tools
    Also revamped footer layout.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,282 +1,230 @@
 import Link from "next/link";
+import Image from "next/image";
+import CloudpressLogo from "../public/cloudpress-logo-square-white.svg";
+import React from "react";
+import {
+  SocialGitHubIcon,
+  SocialLinkedInIcon,
+  SocialXIcon,
+  SocialYoutubeIcon,
+} from "./icons";
 
-export default function Footer() {
+interface NavHeaderProps {
+  text: string;
+}
+function NavHeader({ text }: NavHeaderProps): JSX.Element {
+  return <h3 className="text-sm font-semibold leading-6 text-white">{text}</h3>;
+}
+
+interface NavItemProps {
+  name: string;
+  href: string;
+}
+
+function NavItem({ name, href }: NavItemProps): JSX.Element {
   return (
-    <div className="bg-gray-800">
-      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div className="grid grid-cols-2 xl:grid-cols-3 gap-8">
-          <div className="grid grid-cols-2 xl:grid-cols-1 col-span-2 gap-8 xl:col-span-1">
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Product
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/features"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Features
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/pricing"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Pricing
-                  </Link>
-                </li>
-              </ul>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase mt-8">
-                Resources
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/docs"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Documentation
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/blog"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Blog
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/changelog"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Changelog
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Use Cases
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/use-cases/content-managers"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Content Managers
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/use-cases/content-writers"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Content Writers
-                  </Link>
-                </li>
-              </ul>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase mt-8">
-                Other
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/affiliates"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Affiliate Program
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-8">
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Google Docs Integrations
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/integrations/export-google-docs-to-contentful"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Google Docs to Contentful
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-google-docs-to-kontent-ai"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Google Docs to Kontent.ai
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-google-docs-to-sanity"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Google Docs to Sanity
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-google-docs-to-webflow"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Google Docs to Webflow
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-google-docs-to-wordpress"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Google Docs to WordPress
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Notion Integrations
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/integrations/export-notion-to-contentful"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Notion to Contentful
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-notion-to-kontent-ai"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Notion to Kontent.ai
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-notion-to-sanity"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Notion to Sanity
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-notion-to-webflow"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Notion to Webflow
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/integrations/export-notion-to-wordpress"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Export Notion to WordPress
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-8">
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Contact
-              </h4>
-              <p className="mt-2 text-gray-300 text-base leading-6">
+    <li key={name}>
+      <Link
+        href={href}
+        className="text-sm leading-6 text-gray-300 hover:text-white"
+      >
+        {name}
+      </Link>
+    </li>
+  );
+}
+
+interface SocialLinkProps {
+  name: string;
+  href: string;
+  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+}
+
+const socialLinks: SocialLinkProps[] = [
+  {
+    name: "X",
+    href: "https://twitter.com/usecloudpress",
+    icon: SocialXIcon,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@usecloudpress",
+    icon: SocialYoutubeIcon,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/company/usecloudpresscom",
+    icon: SocialLinkedInIcon,
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/usecloudpress",
+    icon: SocialGitHubIcon,
+  },
+];
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="bg-gray-900" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="space-y-8">
+            <Image
+              className="h-12 w-12"
+              src={CloudpressLogo}
+              alt="Cloudpress logo"
+            />
+            <p className="text-sm leading-6 text-gray-300">
+              Export content from Google Docs and Notion to WordPress, Webflow,
+              Contentful, Sanity.io, and more...
+            </p>
+            <div className="flex space-x-6">
+              {socialLinks.map((item) => (
                 <a
-                  href="mailto:hello@usecloudpress.com"
-                  className="text-base leading-6 text-gray-300 hover:text-white"
+                  key={item.name}
+                  href={item.href}
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                  className="text-gray-500 hover:text-gray-400"
                 >
-                  hello@usecloudpress.com
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
-              </p>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase mt-8">
-                Legal
-              </h4>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <Link
-                    href="/privacy"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Privacy
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/terms"
-                    className="text-base leading-6 text-gray-300 hover:text-white"
-                  >
-                    Terms
-                  </Link>
-                </li>
-              </ul>
+              ))}
             </div>
-            <div>
-              <h4 className="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase">
-                Address
-              </h4>
-              <p className="mt-2 text-gray-300 text-base leading-6">
-                Unit 346, 3/F, Peninsula Centre
-                <br />
-                67 Mody Road, Tsim Sha Tsui
-                <br />
-                Kowloon, Hong Kong
-                <br />
-              </p>
+          </div>
+          <div className="mt-16 grid md:grid-cols-3 gap-8 xl:col-span-2 xl:mt-0 items-start">
+            <div className="grid gap-8">
+              <div>
+                <NavHeader text="Product" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem name="Features" href="/features" />
+                  <NavItem name="Pricing" href="/pricing" />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Resources" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem name="Documentation" href="/docs" />
+                  <NavItem name="Blog" href="/blog" />
+                  <NavItem name="Changelog" href="/changelog" />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Tools" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="Export Google Docs to HTML"
+                    href="/tools/convert/google-docs-to-html"
+                  />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Use Cases" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="Content Managers"
+                    href="/use-cases/content-managers"
+                  />
+                  <NavItem
+                    name="Content Writers"
+                    href="/use-cases/content-writers"
+                  />
+                </ul>
+              </div>
+            </div>
+            <div className="grid gap-8">
+              <div>
+                <NavHeader text="Google Docs Integrations" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="Export Google Docs to Contentful"
+                    href="/integrations/export-google-docs-to-contentful"
+                  />
+                  <NavItem
+                    name="Export Google Docs to Kontent.ai"
+                    href="/integrations/export-google-docs-to-kontent-ai"
+                  />
+                  <NavItem
+                    name="Export Google Docs to Sanity"
+                    href="/integrations/export-google-docs-to-sanity"
+                  />
+                  <NavItem
+                    name="Export Google Docs to Webflow"
+                    href="/integrations/export-google-docs-to-webflow"
+                  />
+                  <NavItem
+                    name="Export Google Docs to WordPress"
+                    href="/integrations/export-google-docs-to-wordpress"
+                  />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Notion Integrations" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="Export Notion to Contentful"
+                    href="/integrations/export-notion-to-contentful"
+                  />
+                  <NavItem
+                    name="Export Notion to Kontent.ai"
+                    href="/integrations/export-notion-to-kontent-ai"
+                  />
+                  <NavItem
+                    name="Export Notion to Sanity"
+                    href="/integrations/export-notion-to-sanity"
+                  />
+                  <NavItem
+                    name="Export Notion to Webflow"
+                    href="/integrations/export-notion-to-webflow"
+                  />
+                  <NavItem
+                    name="Export Notion to WordPress"
+                    href="/integrations/export-notion-to-wordpress"
+                  />
+                </ul>
+              </div>
+            </div>
+            <div className="grid gap-8">
+              <div>
+                <NavHeader text="Contact" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="hello@usecloudpress.com"
+                    href="mailto:hello@usecloudpress.com"
+                  />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Legal" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem name="Privacy" href="/privacy" />
+                  <NavItem name="Terms" href="/terms" />
+                </ul>
+              </div>
+              <div>
+                <NavHeader text="Affiliates" />
+                <ul role="list" className="mt-2 space-y-3">
+                  <NavItem
+                    name="Cloudpress Affiliate Program"
+                    href="/affiliates"
+                  />
+                  <NavItem name="Affiliate Terms" href="/affiliates/terms" />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-        <div className="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
-          <div className="flex md:order-2">
-            <a
-              href="https://twitter.com/usecloudpress"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ml-6 text-gray-400 hover:text-gray-300"
-            >
-              <span className="sr-only">Twitter</span>
-              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-              </svg>
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCo13PRFISQnV0g6jOApFzHw"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ml-6 text-gray-400 hover:text-gray-300"
-            >
-              <span className="sr-only">YouTube</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-              >
-                <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
-              </svg>
-            </a>
-          </div>
-          <p className="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
+        <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
+          <p className="text-xs leading-5 text-gray-400">
             &copy; {new Date().getFullYear()} Cloudpress Limited. All rights
             reserved.
           </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
