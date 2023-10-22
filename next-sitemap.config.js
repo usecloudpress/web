7f404757b15fc841107bf3d1345e57332880885a
Jerrie Pelser <jerrie@jerriepelser.com>
2023-10-22 13:11:45 +0700

    Ensure tools are added to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,20 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.usecloudpress.com",
   generateRobotsTxt: true,
+  additionalPaths: () => {
+    return [
+      {
+        loc: "/tools/convert/google-docs-to-html",
+        changefreq: "daily",
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: "/tools/convert/google-docs-to-markdown",
+        changefreq: "daily",
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+  },
 };
